1c800ea30daffff4ce70d5edf86c7373d108efa3
Roberta Silva <robertadasilva.contato@gmail.com>
2025-08-12 21:49:19 -0300

    Inicial sobre DOM

---
+++ dom-inicial/script.js
@@ -0,0 +1,21 @@
+window.alert("alo");
+
+const h1Selected = document.querySelector("h1");
+const h1Classes = h1Selected.classList;
+
+function callback() {
+  console.log("Clicou em", h1Selected.innerText);
+}
+
+h1Selected.addEventListener("click", callback);
+
+// Retorne o url da página atual utilizando o objeto window
+const href = location.href;
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const ativo = document.querySelector(".ativo");
+// Retorne a linguagem do navegador
+const linguagem = navigator.language;
+// Retorne a largura da janela
+const largura = outerWidth;
+window.outerWidth;

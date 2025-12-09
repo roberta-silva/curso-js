3b27981a66c11db57fa032d53d4c1f919778f064
Roberta Silva <robertadasilva.contato@gmail.com>
2025-12-09 19:10:31 -0300

    number e math 2/2

--- objetos-tipos/number-e-math/script.js
+++ objetos-tipos/number-e-math/script.js
@@ -79,3 +79,38 @@ console.log(aleatorio);
 // um numero aleatorio de 20 a 40
 const aleatorio1 = Math.floor(Math.random() * (40 - 20 + 1)) + 20;
 console.log(aleatorio1);
+
+//exercicios
+// Retorne um número aleatório
+// entre 1050 e 2000
+const numeroAleatorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050);
+console.log(numeroAleatorio);
+
+// Retorne o maior número da lista abaixo
+const numeros = "4, 5, 20, 8, 9";
+const arrayNumeros = numeros.split(", ");
+console.log(arrayNumeros);
+const max = Math.max(...arrayNumeros);
+console.log(max);
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ["R$ 59,99", " R$ 100,222", "R$ 230  ", "r$  200"];
+
+function limparPreco(preco) {
+  preco = +preco.toUpperCase().replace("R$", "").trim().replace(",", ".");
+  preco = +preco.toFixed(2);
+  return preco;
+}
+
+let soma = 0;
+listaPrecos.forEach((item) => {
+  soma += limparPreco(item);
+});
+console.log(soma);
+console.log(
+  soma.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+);
+
+limparPreco(listaPrecos[0]);

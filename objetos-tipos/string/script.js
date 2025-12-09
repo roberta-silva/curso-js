7f7d410a56caaddfead2518854e52ed4c693cbe6
Roberta Silva <robertadasilva.contato@gmail.com>
2025-12-09 16:50:30 -0300

    string 3/3

--- objetos-tipos/string/script.js
+++ objetos-tipos/string/script.js
@@ -107,3 +107,85 @@ const valor = "  R$ 23.00   ";
 valor.trim(); // 'R$ 23.00'
 valor.trimStart(); // 'R$ 23.00   '
 valor.trimEnd(); // '  R$ 23.00'
+
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+console.log("exercicios ------------------------");
+
+const transacoes = [
+  {
+    descricao: "Taxa do Pão",
+    valor: "R$ 39",
+  },
+  {
+    descricao: "Taxa do Mercado",
+    valor: "R$ 129",
+  },
+  {
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 99",
+  },
+  {
+    descricao: "Taxa do Banco",
+    valor: "R$ 129",
+  },
+  {
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 49",
+  },
+];
+
+let taxaTotal = 0;
+let recebimentoTotal = 0;
+transacoes.forEach((item) => {
+  const numeroLimpo = +item.valor.replace("R$", "");
+  if (item.descricao.slice(0, 4) === "Taxa") {
+    taxaTotal = taxaTotal + numeroLimpo;
+  } else if (item.descricao.slice(0, 4) === "Rece") {
+    recebimentoTotal += numeroLimpo;
+  }
+});
+
+console.log(taxaTotal);
+console.log(recebimentoTotal);
+
+// Retorne uma array com a lista abaixo
+const transportes = "Carro;Avião;Trem;Ônibus;Bicicleta";
+const arrayTransportes = transportes.split(";");
+console.log(arrayTransportes);
+
+// Substitua todos os span's por a's
+let html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              </ul>`;
+
+html = html.split("span").join("a");
+console.log(html);
+
+// Retorne o último caracter da frase
+const frase3 = "Melhor do ano!";
+console.log(frase3[frase3.length - 1]);
+console.log(frase3.slice(-1)); //último caracter
+console.log(frase3.slice(-2)); //dois últimos caracteres
+
+// Retorne o total de taxas
+const transacoes2 = [
+  "Taxa do Banco",
+  "   TAXA DO PÃO",
+  "  taxa do mercado",
+  "depósito Bancário",
+  "TARIFA especial",
+];
+
+let taxasTotal = 0;
+transacoes2.forEach((item) => {
+  item = item.toLowerCase(); //tudo minusculo
+  item = item.trim(); //remove espaços em branco do início e do fim da string
+  item = item.slice(0, 4);
+
+  if (item === "taxa") taxasTotal++;
+  console.log(item);
+});
+console.log(taxasTotal);
